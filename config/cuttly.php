e3053a8a1fa8c32bb5a8e956ce226a6dcb1ff2a5
Technoyer <36210275+Technoyer@users.noreply.github.com>
2022-01-07 04:50:34 +0200

    create config file

---
+++ config/cuttly.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Config file...
+ * By: Technoyer Solutions Ltd
+ * Author: Mohamed Gomaa <technoyer@gmail.com>
+ * Company URL: www.technoyer.com
+ */
+
+return [
+    /**
+     * ++++++++++++++++++++++++++++++++
+     * API KEY
+     * +++++++++++++++++++++++++++++++
+     * 
+     * Create your API key from cutt.ly itself
+     * @url: https://cutt.ly/edit
+     */
+
+    'api_key' => env('CUTTLY_API_KEY', ''),
+];
