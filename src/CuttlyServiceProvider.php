e11f70a95cb8e3d623b3fa0af09807480be7d3f2
Technoyer <36210275+Technoyer@users.noreply.github.com>
2022-01-07 04:09:33 +0200

    register service provider

--- src/CuttlyServiceProvider.php
+++ src/CuttlyServiceProvider.php
@@ -3,17 +3,22 @@
 namespace Technoyer\Cuttly;
 
 use Illuminate\Support\ServiceProvider;
+use Technoyer\Cuttly\CuttlyServer;
 
 class CuttlyServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
+     * Register cuttly client.
      *
      * @return void
      */
     public function register()
     {
-        //
+        $this->app->singleton('cuttly', function () {
+            return new CuttlyServer($this->app->make('config')->get('cuttly.api_key', ''));
+        });
+
+        $this->app->bind(CuttlyServer::class, 'cuttly');
     }
 
     /**
@@ -23,6 +28,23 @@ class CuttlyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ( ! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $configPath = $this->app->make('path.config');
+
+        $this->publishes([__DIR__ . '/../config/cuttly.php' => $configPath.'/cuttly.php']);
+    }
+
+    /**
+     * provides
+     */
+    public function provides()
+    {
+        return [
+            CuttlyServer::class,
+            'cuttly'
+        ];
     }
 }
